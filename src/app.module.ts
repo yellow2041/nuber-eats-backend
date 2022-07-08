ca2f90e3a60709631ef8c1a2a620126e4b414b32
신지연/(협력사) 사원/한국영업업무혁신팀(jy0731.shin@lgepartner.com) <jeeyeon654@gmail.com>
2022-07-08 09:02:59 +0900

    1.1강 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot()],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    RestaurantsModule,
+  ],
   controllers: [],
   providers: [],
 })
