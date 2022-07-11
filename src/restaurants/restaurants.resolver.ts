db09943110f25e3e0613aa9ffb904be194d0af65
신지연/(협력사) 사원/한국영업업무혁신팀(jy0731.shin@lgepartner.com) <jeeyeon654@gmail.com>
2022-07-11 12:35:36 +0900

    #1 완료

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,18 @@
-import { Query } from '@nestjs/graphql';
+import { Args, Mutation, Query } from '@nestjs/graphql';
 import { Resolver } from '@nestjs/graphql';
+import { createRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver()
+@Resolver(of => Restaurant)
 export class RestaurantsResolver {
-  @Query(() => Boolean)
-  isPizzaGood(): boolean {
+  @Query(returns => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
+  }
+  @Mutation(returns => Boolean)
+  createRestaurant(@Args() createRestaurantDto: createRestaurantDto): boolean {
+    console.log(createRestaurantDto);
     return true;
   }
 }
