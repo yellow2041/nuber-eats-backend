6f30dda49e0855be4b9368266fde1049afa6af76
신지연/(협력사) 사원/한국영업업무혁신팀(jy0731.shin@lgepartner.com) <jeeyeon654@gmail.com>
2022-07-12 12:43:23 +0900

    2.4강 완료. .env설정 및 typeORM 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
@@ -8,7 +10,21 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true,
+      autoSchemaFile: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'jy',
+      password: '1234',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
     }),
     RestaurantsModule,
   ],
