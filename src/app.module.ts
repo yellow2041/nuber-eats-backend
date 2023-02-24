7b688a3eb46d9d9d17dd1a5a0d394edc126489df
Shin Jiyeon <jeeyeon654@gmail.com>
2023-02-24 10:12:50 +0900

    prod 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -49,7 +49,7 @@ import { UploadsModule } from './uploads/uploads.module';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod', 'test'),
+        NODE_ENV: Joi.string().valid('dev', 'production', 'test'),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
